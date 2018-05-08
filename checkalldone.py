718c42095cd7798f0a455b7f4dee092b91a5525d
Sarah How <sehowerter@gmail.com>
2018-05-08 12:36:55 -0400

    update

---
+++ checkalldone.py
@@ -0,0 +1,24 @@
+import re
+import numpy as np
+import glob
+import os
+import sys
+import pandas as pd
+
+
+jsons = glob.glob('jsons/*')
+
+jusers = []
+for j in jsons:
+	jusers.append(re.search('(?<=jsons/).*(?=-)',j).group(0))
+
+for user in jusers:
+	try:
+		userdf = pd.read_csv('ratios/{}-ratios.csv'.format(user))
+		print('    ',len(list(userdf.index))-1,' tweets finished for ',user)
+		os.system('wc jsons/{}-tweets.json'.format(user))
+		print('     ^ left for ',user)
+	except:
+		print('There are no tweets finished for ',user)
+		os.system('wc jsons/{}-tweets.json'.format(user))
+		print('     ^ left for ',user)	
