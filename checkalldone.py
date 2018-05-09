531c2ed1151d08af576e906c4452b715c255f970
Sarah How <sehowerter@gmail.com>
2018-05-08 20:30:57 -0400

    getmichaeltweets

--- checkalldone.py
+++ checkalldone.py
@@ -11,11 +11,11 @@ jsons = glob.glob('jsons/*')
 jusers = []
 for j in jsons:
 	jusers.append(re.search('(?<=jsons/).*(?=-)',j).group(0))
-
+jusers = sorted(jusers)
 for user in jusers:
 	try:
 		userdf = pd.read_csv('ratios/{}-ratios.csv'.format(user))
-		print('    ',len(list(userdf.index)),' tweets finished for ',user)
+		print('--->',len(list(userdf.index)),' tweets finished for ',user)
 		os.system('wc jsons/{}-tweets.json'.format(user))
 	except:
 		print('There are no tweets finished for ',user)
